9bcade5096f2b8b3edcbb8bf12225a4400f5b9a4
chenhancheng <chenhancheng@genew.com>
2022-01-20 17:25:03 +0800

    first commit

---
+++ src/test/index.js
@@ -0,0 +1,3 @@
+const { add } = require("../../dist/Calc");
+
+console.log(add(1, 2).add(3));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const TerserPlugin = require("terser-webpack-plugin");
+module.exports = {
+    entry: {
+        Calc: "./src/index.js",
+        "Calc.min": "./src/index.js"
+    },
+    output: {
+        filename: "[name].js",
+        library: "calc",
+        libraryTarget: "umd",
+        globalObject: "this"
+    },
+    mode: "none",
+    optimization: {
+        //这个字段很强大，我们做webpack的代码分割，摇数，tree shake等都会用到这个字段
+        minimize: true, //开启插件
+        minimizer: [
+            new TerserPlugin({
+                test: /\.min.js/ //提供一个正则，表示符合有min.js的就进行压缩
+            })
+        ]
+    }
+};
